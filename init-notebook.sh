21cdce1fb820d7644529ab8016e3e88909e717a8
Nicolas Laval <nicolas.laval@making-sense.info>
2024-06-26 09:29:03 +0200

    Fix configuration

--- init-notebook.sh
+++ init-notebook.sh
@@ -13,9 +13,10 @@ cd ./SPARQL-Jupyter-Training
 rm LICENSE
 rm init-notebook.sh
 rm README.md
-rm requirement.txt
+rm requirements.txt
 
 # Open the relevant notebook when starting Jupyter Lab
 jupyter server --generate-config
 echo "c.LabApp.default_url = '/lab/tree/SPARQL-Jupyter-Training/${IPYNB_PATH%.*}/${IPYNB_PATH}'" >> $HOME/.jupyter/jupyter_server_config.py
 echo "c.IPKernelApp.capture_fd_output = False" >> $HOME/.jupyter/jupyter_server_config.py
+echo "c.MultiKernelManager.default_kernel_name = 'ipykernel'" >> $HOME/.jupyter/jupyter_server_config.py
